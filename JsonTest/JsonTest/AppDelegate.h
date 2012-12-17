a5be4b4695226b8060764b8db55971c6b17d6c06
liuwei <liuw79@gmail.com>
2012-12-17 21:27:53 +0800

    JSON
    JSON

---
+++ JsonTest/JsonTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  JsonTest
+//
+//  Created by LIU WEI on 12-12-17.
+//  Copyright (c) 2012年 LIU WEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
